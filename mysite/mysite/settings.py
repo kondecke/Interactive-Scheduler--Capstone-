aea64d0d8a8308d8650e5bbd01a9b84c9a53ab1d
kmill227 <kmill227@kent.edu>
2023-02-21 12:11:22 -0500

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config.DJANGO_SECRET
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kbmiller99.pythonanywhere.com', 'localhost']
 
 
 # Application definition
