c1123e6417a508b002800eeb2797994bde6cf405
kmill227 <kmill227@kent.edu>
2023-02-21 12:16:06 -0500

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['./templates'],
+        'DIRS': ['./templates', '/home/kbmiller99/Interactive-Scheduler--Capstone-/mysite/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
