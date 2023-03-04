2ab32c52a3f8c196e14944bfb05042fb562a12a8
kmill227 <kmill227@kent.edu>
2023-03-04 18:22:42 -0500

    create first serializer

--- mysite/scheduler/serializers.py
+++ mysite/scheduler/serializers.py
@@ -1,1 +1,7 @@
 from rest_framework import serializers
+from models import * 
+class eventSerializer(serializers.ModelSerializer): 
+    class Meta: 
+        model = Event 
+        fields = ['eventid', 'time', 'description', 'alert', 'accesslevel']
+
