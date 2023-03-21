4e221cfd4ed4b4a74bc051318b5fa40f89bbc113
Csnook1 <csnook1@kent.edu>
2023-03-21 15:28:43 -0400

    Uninstall system32

--- mysite/scheduler/serializers.py
+++ mysite/scheduler/serializers.py
@@ -26,3 +26,42 @@ class followersSerializer(serializers.ModelSerializer):
         model = models.Followers
         fields = ['userid', 'followingid']
 
+class messagesSerializer(serializers.ModelSerializer):
+    class meta:
+        model = models.Messages
+        fields = ['msgid', 'msgcontent', 'touser', 'fromuser', 'togroup']
+
+class notificationsSerializer(serializers.ModelSerializer):
+    class meta:
+        model = models.Notifications
+        fields = ['notificationid', 'userid', 'notificationmsg']
+
+class postsSerializer(serializers.ModelSerializer):
+    class meta:
+        model = models.Posts
+        fields = ['postid', 'threadid', 'fromuser','threadtitle', 'threaddescription', 'postcontent']
+
+class professorSerializer(serializers.ModelSerializer):
+    class meta:
+        model = models.Professor
+        fields = ['profid', 'email', 'firstname', 'lastname']
+
+class rolesSerializer(serializers.ModelSerializer):
+    class meta: 
+        model = models.Roles
+        fields = ['roleid', 'title', 'level', 'cancreateevent', 'candeleteevent', 'canviewevents']
+
+class studentEventsSerializer(serializers.ModelSerializer):
+    class meta: 
+        model = models.Studentevents
+        fields = ['studentid', 'eventid', 'groupid']
+
+class studentsinGroupSerializer(serializers.ModelSerializer):
+    class meta: 
+        model = models.Studentsingroup
+        fields = ['studentid', 'groupid', 'role']
+
+class userSerializer(serializers.ModelSerializer):
+    class meta: 
+        model = models.User
+        fields = ['studentid', 'email', 'address', 'phonenumber', 'firstname', 'lastname', 'standing']
